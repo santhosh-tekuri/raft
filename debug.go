404489fbd3e9b05f42f173e531dd4ae4a650bdae
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-02-19 14:04:37 +0530

    Fix: tests are timing out because of debug messages
    print debug messages in another goroutine

--- debug.go
+++ debug.go
@@ -5,7 +5,6 @@ package raft
 import (
 	"fmt"
 	"strings"
-	"sync"
 
 	"github.com/fatih/color"
 )
@@ -16,27 +15,32 @@ var colorF = color.New(color.FgCyan)
 var colorR = color.New(color.FgYellow)
 var colorU = color.New(color.FgHiWhite)
 
-var printMu sync.Mutex
+var messages = func() chan string {
+	ch := make(chan string, 10000)
+	go func() {
+		for msg := range ch {
+			switch {
+			case strings.Index(msg, " L | ") != -1:
+				i := strings.Index(msg, " L | ")
+				if strings.HasPrefix(msg[i+6:], ":8888") {
+					colorR.Print(msg)
+				} else {
+					colorL.Print(msg)
+				}
+			case strings.Index(msg, " C | ") != -1:
+				colorC.Print(msg)
+			case strings.Index(msg, " F | ") != -1:
+				colorF.Print(msg)
+			default:
+				colorU.Print(msg)
+			}
+		}
+	}()
+	return ch
+}()
 
 func debug(args ...interface{}) {
-	printMu.Lock()
-	defer printMu.Unlock()
-
-	switch msg := fmt.Sprintln(args...); {
-	case strings.Index(msg, " L | ") != -1:
-		i := strings.Index(msg, " L | ")
-		if strings.HasPrefix(msg[i+6:], ":8888") {
-			colorR.Print(msg)
-		} else {
-			colorL.Print(msg)
-		}
-	case strings.Index(msg, " C | ") != -1:
-		colorC.Print(msg)
-	case strings.Index(msg, " F | ") != -1:
-		colorF.Print(msg)
-	default:
-		colorU.Print(msg)
-	}
+	messages <- fmt.Sprintln(args...)
 }
 
 func assert(b bool, format string, args ...interface{}) {
