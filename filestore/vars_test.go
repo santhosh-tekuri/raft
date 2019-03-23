ab7087e69b6574523b1e981379012d34fdef05f6
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-03-23 15:17:52 +0530

    filestore: add test helpers

--- filestore/vars_test.go
+++ filestore/vars_test.go
@@ -7,6 +7,52 @@ import (
 	"testing"
 )
 
+func TestNewVars(t *testing.T) {
+	tests := []struct {
+		name    string
+		content []byte
+		valid   bool
+	}{
+		{"noFile", nil, true},
+		{"zeroSize", make([]byte, 0), false},
+		{"smallSize", make([]byte, 20), false},
+		{"bigSize", make([]byte, 100), false},
+		{"zeroOff", slice65(0), false},
+		{"wrongOff", slice65(5), false},
+		{"off1", slice65(1), true},
+		{"off33", slice65(33), true},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			fname, err := tempFile(".vars", test.content)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.Remove(fname)
+			v, err := NewVars(fname)
+			if valid := err == nil; valid != test.valid {
+				t.Fatalf("valid: got %v, want %v : %v", valid, test.valid, err)
+			}
+			if test.valid {
+				defer v.Close()
+				if err = ensureVars(v, 0, 0, 0, 0); err != nil {
+					t.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+// helpers ------------------------------------------------------------
+
+// slice65 returns a slice of length 65, with first byte initialized to given value.
+func slice65(first byte) []byte {
+	b := [65]byte{first}
+	return b[:]
+}
+
+// tempFile creates a tempFile with given content, and returns its name.
+// if content is nil, it does not create file, but returns free name.
 func tempFile(pattern string, content []byte) (string, error) {
 	f, err := ioutil.TempFile("", pattern)
 	if err != nil {
@@ -26,58 +72,28 @@ func tempFile(pattern string, content []byte) (string, error) {
 	return f.Name(), nil
 }
 
-func slice(b [65]byte) []byte { return b[:] }
-
-func TestNewVars(t *testing.T) {
-	tests := []struct {
-		name    string
-		content []byte
-		valid   bool
-	}{
-		{"noFile", nil, true},
-		{"zeroSize", make([]byte, 0), false},
-		{"smallSize", make([]byte, 20), false},
-		{"bigSize", make([]byte, 100), false},
-		{"zeroOff", slice([65]byte{0}), false},
-		{"wrongOff", slice([65]byte{5}), false},
-		{"off1", slice([65]byte{1}), true},
-		{"off33", slice([65]byte{33}), true},
+func ensureVars(vars *Vars, cid, nid, term, vote uint64) error {
+	c, n, err := vars.GetIdentity()
+	if err != nil {
+		return fmt.Errorf("getIdentity: %v", err)
 	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			fname, err := tempFile(".vars", test.content)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer os.Remove(fname)
-			v, err := NewVars(fname)
-			if valid := err == nil; valid != test.valid {
-				t.Fatalf("valid: got %v, want %v : %v", valid, test.valid, err)
-			}
-			if test.valid {
-				defer v.Close()
-				cid, nid, err := v.GetIdentity()
-				if err != nil {
-					t.Fatalf("getIdentity: %v", err)
-				}
-				if cid != 0 {
-					t.Fatalf("cid: got %v, want %v", cid, 0)
-				}
-				if nid != 0 {
-					t.Fatalf("cid: got %v, want %v", cid, 0)
-				}
-
-				term, vote, err := v.GetVote()
-				if err != nil {
-					t.Fatalf("getVote: %v", err)
-				}
-				if term != 0 {
-					t.Fatalf("term: got %v, want %v", cid, 0)
-				}
-				if vote != 0 {
-					t.Fatalf("vote: got %v, want %v", cid, 0)
-				}
-			}
-		})
+	if c != cid {
+		return fmt.Errorf("cid: got %v, want %v", c, cid)
 	}
+	if n != nid {
+		return fmt.Errorf("nid: got %v, want %v", n, nid)
+	}
+
+	t, v, err := vars.GetVote()
+	if err != nil {
+		return fmt.Errorf("getVote: %v", err)
+	}
+	if t != term {
+		return fmt.Errorf("term: got %v, want %v", t, term)
+	}
+	if v != vote {
+		return fmt.Errorf("vote: got %v, want %v", v, vote)
+	}
+
+	return nil
 }
