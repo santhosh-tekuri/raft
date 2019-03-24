0f536ea7c62b3d14d4d261be0e91d9d076a86569
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-03-25 02:08:28 +0530

    log: remove unnecessary panic check

--- log/log.go
+++ log/log.go
@@ -203,9 +203,6 @@ func (l *Log) RemoveLTE(i uint64) error {
 
 	s = l.seg
 	if s.prev == nil && s.idx.n > 0 && s.lastIndex() <= i { // clear this segment
-		if s != l.seg {
-			panic("s should be last segment")
-		}
 		next, err := newSegment(l.dir, s.lastIndex()+1, l.maxCount, l.maxSize)
 		if err != nil {
 			return err
