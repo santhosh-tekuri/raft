9ded715bffa06e08531431ab4171a60695bb9048
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-03-30 12:32:22 +0530

    add todo

--- doc.go
+++ doc.go
@@ -9,3 +9,6 @@ package raft
 // todo: resolver should catch latest resolved addr
 // todo: leader should back pressure on newEntryCh
 // todo: can we provide type safe tasks: task.Result() now returns interface{}
+
+// todo: if by any chance we get invalid rpcType in server, dont panic
+//       simply close the connection (during tests it is ok to panic)
