3120b7e62b989ced3fa1a92c61087734d4c17980
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-04-19 23:27:23 +0530

    change error prefix from "configChange" to "changeConfig"

--- errors.go
+++ errors.go
@@ -45,7 +45,7 @@ var (
 	// User can retry ChangeConfig after some time in case of this error.
 	ErrNotCommitReady = temporaryError("raft.configChange: not ready to commit")
 
-	ErrStaleConfig            = errors.New("raft.configChange: submitted config is stale")
+	ErrStaleConfig            = errors.New("raft.changeConfig: submitted config is stale")
 	ErrSnapshotThreshold      = errors.New("raft.takeSnapshot: not enough outstanding logs to snapshot")
 	ErrNoUpdates              = errors.New("raft.takeSnapshot: no updates to FSM")
 	ErrQuorumUnreachable      = errors.New("raft: quorum unreachable")
